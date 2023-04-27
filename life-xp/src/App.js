c44df54fe68e03b1a7d71ca17e54e3fb4021a5a8
LAKSHMI PRASANNA <mgc01official@gmail.com>
2023-04-27 23:49:37 +0530

    changed header style and added cups to the page

--- life-xp/src/App.js
+++ life-xp/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [timeRemaining, setTimeRemaining] = useState(null);
+  const [expPoints, setExpPoints] = useState(2500);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -49,23 +50,92 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="countdown">{timeRemaining}</div>
-        <Expbar />
+        <div className="digits">
+          <div className="exp-points">{expPoints}/100000 EXPERIENCE</div>
+          <div className="logo">--XP--</div>
+          <div className="countdown">{timeRemaining}</div>
+        </div>
+
+        <Expbar expPoints={expPoints} />
       </header>
-      <body></body>
+      <body-2>
+        <Cupboard />
+      </body-2>
     </div>
   );
 }
 
-function Expbar() {
+function Expbar({ expPoints }) {
+  const remainingTime =
+    ((new Date("2028-01-01").getTime() - new Date().getTime()) /
+      new Date("2028-01-01").getTime()) *
+    window.innerWidth;
+  // console.log(expPoints / 10000);
   return (
     <div className="Expbar">
       {/* <div className="expbar-description">Life EXP Points</div> */}
       <div className="expbar-in">
-        <div className="expbar-val" style={{ width: "10%" }}></div>
+        <div
+          className="expbar-val"
+          style={{ width: (expPoints / 100000) * window.innerWidth }}
+        ></div>
+        <div
+          className="expbar-val-time"
+          style={{ width: window.innerWidth - remainingTime }}
+        ></div>
       </div>
     </div>
   );
 }
 
+function Cupboard() {
+  return (
+    <div className="cup-platform">
+      <div
+        class="cup"
+        style={{
+          background:
+            "linear-gradient(to bottom,#00000005,#00000005 70%,green 30%,rgb(0, 0, 0))",
+        }}
+      >
+        <div class="bubble bubble-1"></div>
+        <div class="bubble bubble-2"></div>
+        <div class="bubble bubble-3"></div>
+      </div>
+      <div
+        class="cup"
+        style={{
+          background:
+            "linear-gradient(to bottom,#00000005,#00000005 60%,orange 40%,rgb(0, 0, 0))",
+        }}
+      >
+        <div class="bubble bubble-1"></div>
+        <div class="bubble bubble-2"></div>
+        <div class="bubble bubble-3"></div>
+      </div>
+      <div
+        class="cup"
+        style={{
+          background:
+            "linear-gradient(to bottom,#00000005,#00000005 80%,blue 20%,rgb(0, 0, 0))",
+        }}
+      >
+        <div class="bubble bubble-1"></div>
+        <div class="bubble bubble-2"></div>
+        <div class="bubble bubble-3"></div>
+      </div>
+      <div
+        class="cup"
+        style={{
+          background:
+            "linear-gradient(to bottom,#00000005,#00000005 60%,cyan 40%,rgb(0, 0, 0))",
+        }}
+      >
+        <div class="bubble bubble-1"></div>
+        <div class="bubble bubble-2"></div>
+        <div class="bubble bubble-3"></div>
+      </div>
+    </div>
+  );
+}
 export default App;
